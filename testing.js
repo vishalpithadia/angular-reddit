f6c432493b9ad2a7a27509cc7916a1a8cb0b8381
Vishal Pithadya <vishalpithadia78@gmail.com>
2022-11-30 09:39:18 +0530

    first commit

---
+++ testing.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.HelloClass = void 0;
+var HelloClass = /** @class */ (function () {
+    function HelloClass() {
+    }
+    HelloClass.prototype.hello = function (name) {
+        return 12;
+    };
+    return HelloClass;
+}());
+exports.HelloClass = HelloClass;
